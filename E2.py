18efe76bad8bc91e1e27705f067720df77e5cd34
Azsu-rae <aitameurmedilyas@gmail.com>
2026-03-02 11:58:21 +0100

    init

---
+++ E2.py
@@ -0,0 +1,19 @@
+import time
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+N = 100000
+start = time.time()
+primes = [x for x in range(2, N + 1) if is_prime(x)]
+end = time.time()
+
+print(f"Number of primes found: {len(primes)}")
+print(f"Elapsed time (python): {end - start:.6f} seconds")
---
+++ test.py
@@ -0,0 +1,4 @@
+primes = [i for i in range(0, 1000)]
+
+primes.remove(4)
+print(primes)
