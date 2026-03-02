a96ef60e3721a24cdac7cd5217cc8d7101ad5dc8
Azsu-rae <aitameurmedilyas@gmail.com>
2026-03-02 23:49:52 +0100

    sync

--- E2.py
+++ E2.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 def is_prime_naive(n):
     if n < 2:
@@ -9,23 +10,40 @@ def is_prime_naive(n):
     return True
 
 
-def is_prime(n):
+def is_prime_math(n):
     if n < 2:
         return False
-    for i in range(2, int(sqrt(n))):
+    for i in range(2, int(math.sqrt(n))):
         if n % i == 0:
             return False
     return True
 
 
 N = 100000
+numbers = [x for x in range(0, N+1)]
 start = time.time()
-primes = [x for x in range(2, N + 1) if is_prime_naive(x)]
+non_opt_primes = [x for x in numbers if is_prime_naive(x)]
 end = time.time()
 non_optimal = end - start
 
 start = time.time()
+for num in numbers:
+    if num != 0 and is_prime_math(num):
+        factor = 2
+        multiple = num * factor
+        while multiple < N+1:
+            numbers[multiple] = 0
+            factor += 1
+            multiple = num * factor
 end = time.time()
+optimal = end - start
+opt_primes = [x for x in numbers if x >= 2 and x != 0]
 
-print(f"Number of primes found: {len(primes)}")
-print(f"Elapsed time (python): {end - start:.6f} seconds")
+print(f"Number of primes found:")
+print(f"optimal: {len(opt_primes)}")
+print(f"non_optimal: {len(non_opt_primes)}")
+
+print(f"Elapsed time (python):")
+print(f"optimal: {optimal}")
+print(f"non_optimal: {non_optimal}")
+print(f"ratio: {non_optimal / optimal}")
--- test.py
+++ test.py
@@ -7,5 +7,7 @@ primes.remove(4)
 if 4 in primes:
     primes.remove(4)
 
-print(int(math.sqrt(4)))
+for i in range(2, 0):
+    print("I seem to be doing something")
 
+print(int(math.sqrt(4)))
